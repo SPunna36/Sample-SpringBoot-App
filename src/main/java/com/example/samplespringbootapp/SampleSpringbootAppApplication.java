814410d98620ba627a1e09a925e57294928267ac
Srikanth Punna <SPunna36@massmutual.com>
2021-05-29 17:33:53 +0530

    Main Class Modified.

--- src/main/java/com/example/samplespringbootapp/SampleSpringbootAppApplication.java
+++ src/main/java/com/example/samplespringbootapp/SampleSpringbootAppApplication.java
@@ -2,12 +2,13 @@ package com.example.samplespringbootapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SampleSpringbootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleSpringbootAppApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(SampleSpringbootAppApplication.class, args);
+		run.close();
 	}
-
 }
